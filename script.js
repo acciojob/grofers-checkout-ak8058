0c9ffde01a0d5ff75cf0ca3f8a43be67bc8a43ff
Anil Kumar <114513976+ak8058@users.noreply.github.com>
2025-02-11 16:13:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices=document.querySelectorAll(".price");
+	let total=0;
+	prices.forEach(price=>{
+		total=total+parseFloat(price.textContent);
+	});
+
+	let existingTotalRow=document.getElementById("totalRow");
+	if(existingTotalRow){
+		existingTotalRow.remove();
+	}
+	const totalRow=document.createElement("tr");
+	totalRow.id="totalRow";
+
+	const totalCell=document.createElement("td");
+	totalCell.colSpan=2;
+	totalCell.style.fontWeight = "bold";
+    totalCell.style.textAlign = "center";
+    totalCell.textContent = "Total Price: ₹" + total;
+
+	totalRow.appendChild(totalCell);
+
+	document.querySelector("table").appendChild(totalRow);
   
 };
 
